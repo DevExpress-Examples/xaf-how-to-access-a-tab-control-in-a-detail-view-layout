f7e20a76d2b6ba32d7968f9bef699987f7f02d40
Anastasiya Kisialeva <67680216+anastasiakiseleva@users.noreply.github.com>
2026-05-19 12:20:13 +0200

    Remove UseOldTemplates from WinApplication constructor
    Removed the UseOldTemplates property from the constructor.

--- CS/EFCore/AccessLayoutEF/AccessLayoutEF.Win/WinApplication.cs
+++ CS/EFCore/AccessLayoutEF/AccessLayoutEF.Win/WinApplication.cs
@@ -18,7 +18,6 @@ public class AccessLayoutEFWindowsFormsApplication : WinApplication {
 		SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
         ApplicationName = "AccessLayoutEF";
         CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
-        UseOldTemplates = false;
         DatabaseVersionMismatch += AccessLayoutEFWindowsFormsApplication_DatabaseVersionMismatch;
         CustomizeLanguagesList += AccessLayoutEFWindowsFormsApplication_CustomizeLanguagesList;
     }
